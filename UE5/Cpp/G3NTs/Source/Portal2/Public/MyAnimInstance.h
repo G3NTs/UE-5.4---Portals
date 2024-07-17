7901be7758bec1f14ba86aaa11d04750957d0747
G3NTs <122412059+G3NTs@users.noreply.github.com>
2024-07-17 11:54:29 +0200

    Update MyAnimInstance.h
    comments to UMyAnimationInstance

--- UE5/Cpp/G3NTs/Source/Portal2/Public/MyAnimInstance.h
+++ UE5/Cpp/G3NTs/Source/Portal2/Public/MyAnimInstance.h
@@ -7,7 +7,7 @@
 #include "MyAnimInstance.generated.h"
 
 /**
- * 
+ * This animation instance is used for the player animation syncing. It contains floats and bools that are used by the animation blueprint to determine its animation state. These are synced through the PortalManager class.
  */
 UCLASS()
 class PORTAL2_API UMyAnimInstance : public UAnimInstance
